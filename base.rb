0d43cdda750eda0af89f43359a2ab8c28f136df9
瀬長顕穂 <senri7@senagatakeshiho-no-MacBook-Pro.local>
2018-08-14 20:54:59 +0900

    base.rbを作ってみた

---
+++ base.rb
@@ -0,0 +1,22 @@
+class base
+	# Arrayを１つ持たせる
+	box = Array.new
+
+	# 継承したClassはこれを呼び出す(push)
+	def push
+		push_proc
+	end
+	# これを子クラスでオーバーライドしないとpushから呼ばれてエラーになる
+	def push_proc
+		raise "call abstract !"
+	end
+
+	# 継承したClassはこれを呼び出す(pop)
+	def pop
+		push_proc
+	end
+	# これを子クラスでオーバーライドしないとpopから呼ばれてエラーになる
+	def pop_proc
+		raise "call abstract !"
+	end
+end
