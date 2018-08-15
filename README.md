3f63e359aa6263b1dace1ea810bf49c200541ca6
瀬長顕穂 <senri7@senagatakeshiho-no-MacBook-Pro.local>
2018-08-15 14:07:45 +0900

    mainの振る舞い追加,READMEかきました

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ruby
+queue.rb のクラスが「Queue」と作成しようとしたがエラーがでたので(すでに存在している？)
+のでクラス名「Ueue」にしています
+
+baseに属性として配列を持たせて，stack,queueに継承させているつもりです。
+ですが，baseにそのままArrayを継承させて,それをまた継承させるのか．改善できるのかまで確認できてません。
+
+baseのbox(継承させたいArray)の継承のさせ方が正しいかわかりませんでした。↓を参考にしました
+https://qiita.com/metheglin/items/094022937fba1a17418f
+
+queueとstackのプッシュ動作が，一緒でいいなかなと思ったので，
+baseのプッシュをそのまま使ってます。↓を参考にしました
+https://qiita.com/jnchito/items/62b9bab0455ed0f47d19
+
+社員モデル(member)は，宣言のときに引数をもらって作成するようにしています
--- main.rb
+++ main.rb
@@ -7,20 +7,9 @@ queue.push(Member.new("田中",22,"男"))
 queue.push(Member.new("中谷",32,"女"))
 queue.pop
 queue.push(Member.new("瀬長",21,"男"))
-stack.push(Member.new("田中",22,"男"))
-
-# loop do 
-#     puts "番号を選んでください0:終わる 1:キューにプッシュ 2:キューをポップ 3:スタックにプッシュ 4:スタックをポップ"
-#  	judge = gets.to_i
-#  	if judge == 1 then
-#  		queue.push(Member.new)
-#  	elsif judge == 2 then
-#  		queue.pop
-#  	elsif judge == 3 then
-#  		stack.push(Member.new)
-#  	elsif judge == 4 then
-#  		stack.pop
-#  	end
-#  	# 半角数字以外ならループ
-# 	break if judge ==0
-# end
+queue.pop
+stack.push(Member.new("鈴木",50,"女"))
+stack.push(Member.new("砂糖",41,"男"))
+stack.pop
+stack.push(Member.new("宮里",20,"女"))
+stack.pop
